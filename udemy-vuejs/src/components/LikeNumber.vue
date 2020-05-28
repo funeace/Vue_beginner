40161920ef268d3216e35cc3db611ebff30081f9
Kengo <funeace@gmail.com>
2020-05-28 18:48:32 +0900

    [Add]コンポーネント_section1

---
+++ udemy-vuejs/src/components/LikeNumber.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- templateの要素は１個じゃないといけない -->
+  <!-- divとかでグループ化してあげればいい -->
+  <div>
+    <p>いいね({{number}})</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+  // export defaultの中にコンポーネントオプションを設置する
+  export default{
+    data: function() {
+      return {
+        number: 5
+      }
+    },
+    methods: {
+      increment: function(){
+        this.number += 1
+      }
+    }
+  }
+</script>
+// 普通に書くと、style要素は全てのタグに適応されてしまう。
+// 特定の部分だけ CSSを適応させるには、scopedを付与する
+<style scoped>
+  div{
+    border: 1px solid blue;
+  }
+</style>
--- udemy-vuejs/src/main.js
+++ udemy-vuejs/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 作成したコンポーネントはmain.jsにインポートすることでどこからでも利用できるようになる(グローバル)
+import LikeNumber from './components/LikeNumber.vue'
 
 Vue.config.productionTip = false
+
+// インポートしたデータをLikeNumberで呼び出せるように定義
+Vue.component('LikeNumber',LikeNumber)
 
 // index.htmlの#appを指定
 new Vue({
