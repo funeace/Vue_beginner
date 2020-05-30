5ee313a380bad6ada174df7fbd9844207ef36d81
Kengo <funeace@gmail.com>
2020-05-30 17:15:48 +0900

    [Add]フォームの作り方

--- udemy-vuejs/src/App.vue
+++ udemy-vuejs/src/App.vue
@@ -1,18 +1,87 @@
 <template>
   <div>
+    <div>
+      <EventTitle v-model="eventData.title"></EventTitle>
+      <EventTitle v-model="eventData.title"></EventTitle>
+
+      <p>開催場所</p>
+      <select v-model="eventData.locations">
+        <option v-for="location in locations" :key="location">{{ location }}</option>
+      </select>
+      <p>{{ eventData.location }}</p>
+    </div>
+
+
+    <!-- radioボタンの練習 -->
+    <div>
+      <p>参加費</p>
+      <input type="radio" id="free" value="無料" v-model="eventData.price">
+      <label for="free">無料</label>
+
+      <input type="radio" id="payed" value="有料" v-model="eventData.price">
+      <label for="payed">有料</label>
+    </div>
+
+
+    <div>
+      <!-- チェックボックスの練習(複数)
+      配列にすればデータを受け取れる -->
+      <p>参加条件</p>
+      <input type="checkbox" id="10" value="１０代" v-model="eventData.target">
+      <label for="10">10代</label>
+
+      <input type="checkbox" id="20" value="２０代" v-model="eventData.target">
+      <label for="10">20代</label>
+
+      <input type="checkbox" id="30" value="３０代" v-model="eventData.target">
+      <label for="10">30代</label>
+
+      <p>{{ eventData.target}}</p>
+    </div>
+    <div>
+      <!-- チェックボックスの練習(単体) -->
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <label for="isPrivate">非公開</label>
+      <p>{{ eventData.isPrivate}}</p>
+    </div>
+
+      <label for="detail">複数行のv-model練習</label>
+      <textarea id="detail" rows="10" cols="30" v-model="eventData.detail">
+      </textarea>
+      <pre>{{ eventData.detail }}</pre>
+
+      <label for="host">trim修飾子の練習</label>
+      <input type="text" id="host" v-model.number="eventData.host">
+      <pre>{{eventData.host}}</pre>
+
+      <!--  -->
+      <label for="maxNumber">max修飾子の練習 .numberをつけなければstringで追加されてしまう</label>
+      <input type="number" id="maxNumber" v-model.number="eventData.maxNumber">
+      <p>{{ typeof eventData.maxNumber}}</p>
+
+
+      <label for="title">lazy修飾子は、フォーカスが外れたタイミングで動く</label>
+      <input type="text" id="title" v-model.lazy="lazyTest.title">
+      <p>{{ lazyTest.title}}</p>
+
+    <div>
+      <h2>イベントのフォーム</h2>
+      <label for="title">タイトル</label>
+      <input type="text" id="title" v-model="eventData.title">
+      <p>{{eventData.title}}</p>
+    </div>
     <!-- 動的コンポーネント -->
     <button @click="currentComponent = 'Home'">Home</button>
     <button @click="currentComponent = 'About'">About</button>
-    <!-- 以下の処理はcomponentタグに置き換えることができる -->
-    <!-- <About v-if="currentComponent === 'About'"></About>
-    <Home v-else></Home> -->
-    <!-- 動的なコンポーネントはcomponent :is="コンポーネント名"でおける
-    keep-aliveを使うと、動的コンポーネントでデータを削除されないようにする -->
+    <!--  -->
+    <About v-if="currentComponent === 'About'"></About>
+    <Home v-else></Home>
+    
+    <!--  -->
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
-    <hr>
-    <!--  -->
+
     <LikeHeader header-text="hello">
       <h2>みなさん</h2>
       <!-- v-slotは、templateに定義し、処理を中に入れる。第1引数に名前をつけ、子コンポーネント側で呼び出す
@@ -42,18 +111,36 @@
 import LikeHeader from "./components/LikeHeader.vue"
 import Home from "./components/Home.vue"
 import About from "./components/About.vue"
+import EventTitle from "./components/EventTitle.vue"
 
 export default {
   data() {
     return{
       number: 10,
-      currentComponent: "Home"
+      currentComponent: "Home",
+      eventData: {
+        title: "タイトル",
+        maxNumber: 0,
+        host: "太郎",
+        detail: "Detailの練習です",
+        isPrivate: false,
+        // チェックボックス複数の場合は配列になる
+        target: [],
+        price: "無料",
+        locations: ["東京","大阪","名古屋"],
+        location: "東京"
+      },
+      props: ["value"],
+      lazyTest: {
+        title: "lazyテスト"
+      }
     }
   },
   components: {
     LikeHeader: LikeHeader,
-    About: About,
-    Home: Home
+    About,
+    Home,
+    EventTitle
   }
 }
 </script>
